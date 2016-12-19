ed48ccce34f1775f5bc31be0db1519ccb5dedf5b
SpiritCroc <spiritcroc@gmail.com>
2016-12-19 17:42:37 +0100

    AICPfy
    Change-Id: I7eabe29bfb7110ad664ef84279a7993b6beb0836

--- aicp.mk
+++ aicp.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo aosp_sgp521-userdebug
+# Assert
+TARGET_OTA_ASSERT_DEVICE := SGP521,SGP541,SGP551,castor
+
+# Inherit AOSP Shinano common device parts
+$(call inherit-product, device/sony/castor/aosp_sgp521.mk)
+
+# Inherit AICP castor common device parts
+$(call inherit-product, device/sony/castor_windy/aicp_common.mk)
+
+# Override Product Name for AICP
+PRODUCT_NAME := aicp_castor
+PRODUCT_MODEL := Xperia Z2 Tablet
